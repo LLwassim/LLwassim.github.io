3715284599d79325660b058f3a23d52fecaf7779
Wassim Lkorchy <lacorchy77@gmail.com>
2019-12-03 14:34:08 -0500

    added sotrybook

---
+++ static_storybook/precache-manifest.cc0ad41ecc2428c898e46a9e05b94b31.js
@@ -0,0 +1,59 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef074fabd5626c2788c951828c7e4d8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.94e81e0cffa6a172b0ac.bundle.js"
+  },
+  {
+    "revision": "827e86a589e8414961db",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.94e81e0cffa6a172b0ac.bundle.js"
+  },
+  {
+    "revision": "70235270d5036f923eab23fda958a82d",
+    "url": "static/media/add.70235270.svg"
+  },
+  {
+    "revision": "46ac6c9e435c0aac0876ff2efb161143",
+    "url": "static/media/beats.46ac6c9e.png"
+  },
+  {
+    "revision": "64e74253f058dc668ad38fc64c800038",
+    "url": "static/media/cart_2.64e74253.svg"
+  },
+  {
+    "revision": "0ae49bb896d2b79458854626af2d38db",
+    "url": "static/media/checkmark.0ae49bb8.svg"
+  },
+  {
+    "revision": "cce5b434e5d3292b940b8f39bcb56216",
+    "url": "static/media/hearts.cce5b434.svg"
+  },
+  {
+    "revision": "886209125a5c580e6626fef56a970f1d",
+    "url": "static/media/mac.88620912.png"
+  },
+  {
+    "revision": "a4bc1b3bf98f5d52fd18098396cf63d1",
+    "url": "static/media/minus.a4bc1b3b.svg"
+  },
+  {
+    "revision": "f8ce72af7cb60bed6f65146e89c9d459",
+    "url": "static/media/star.f8ce72af.svg"
+  },
+  {
+    "revision": "9801357361ca2953aecaf9e378de9525",
+    "url": "static/media/white-cart_2.98013573.svg"
+  },
+  {
+    "revision": "bcf238ce2e933551b97469b6a96ef901",
+    "url": "static/media/white-hearts.bcf238ce.svg"
+  },
+  {
+    "url": "vendors~main.94e81e0cffa6a172b0ac.bundle.js"
+  }
+]);
